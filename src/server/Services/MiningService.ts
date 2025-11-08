3c7650ca055e1444f614f747fb5618dc440f0c90
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-11-08 15:19:18 +0800

    feat: add history for late joiners (requires futher testing)

--- src/server/Services/MiningService.ts
+++ src/server/Services/MiningService.ts
@@ -1,5 +1,6 @@
 import { InventoryEvent, MiningEvent } from "../Components/Networks";
 import { PlayerService } from "./PlayerService";
+import { Block } from "@/shared/Block";
 import { Pickaxe } from "@/shared/Pickaxe";
 import { OnStart, Service } from "@flamework/core";
 import Log from "@rbxts/log";
@@ -8,8 +9,11 @@ import Log from "@rbxts/log";
 class MiningService implements OnStart {
 	private static pickaxe = new Map<number, Pickaxe>();
 
+	private static histroy = new Map<string, { position: Vector3; damage: number }>();
+
 	onStart() {
 		MiningEvent.Mine.connect(MiningService.onMine);
+		PlayerService.PlayerJoined.Connect(MiningService.onPlayerJoined);
 		PlayerService.PlayerLeft.Connect(MiningService.onPlayerLeft);
 	}
 
@@ -23,6 +27,22 @@ class MiningService implements OnStart {
 		if (!pickaxe) return Log.Warn(`${player} does not contain a pickaxe`);
 
 		MiningEvent.Mined.except(player, position, pickaxe.damage);
+
+		const key = tostring(Block.InBlockPosition(position));
+		let data = MiningService.histroy.get(key);
+
+		if (!data) {
+			data = { position, damage: 0 };
+			MiningService.histroy.set(key, data);
+		}
+
+		data.damage += pickaxe.damage;
+	};
+
+	private static onPlayerJoined = (player: Player) => {
+		for (const [, { position, damage }] of MiningService.histroy) {
+			MiningEvent.Mined.fire(player, position, damage);
+		}
 	};
 
 	private static onPlayerLeft = (player: Player) => {
