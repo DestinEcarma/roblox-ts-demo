baefe96dd3ad85fc129277c010a02a3637ab3034
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2025-11-10 18:10:42 +0800

    refactor: add `TextLabel` and use this on `Coordinate`

--- src/client/UI/Components/Coordinate.tsx
+++ src/client/UI/Components/Coordinate.tsx
@@ -1,3 +1,4 @@
+import { TextLabel } from "./ui/TextLabel";
 import { character } from "@/client/Components/Character";
 import { Block } from "@/shared/Block";
 import React from "@rbxts/react";
@@ -13,17 +14,14 @@ function Coordinate() {
 	}, []);
 
 	return (
-		<textlabel
+		<TextLabel
 			AnchorPoint={new Vector2(1, 1)}
 			Position={UDim2.fromScale(1, 1)}
-			Size={UDim2.fromOffset(200, 50)}
 			BackgroundTransparency={1}
 			Text={`<b>X: ${position.X}, Y: ${position.Y}, Z: ${position.Z}</b>`}
-			Font="FredokaOne"
-			FontSize="Size24"
-			RichText={true}
-			TextStrokeTransparency={0.75}
-			TextColor3={new Color3(1, 1, 1)}
+			uiStrokeInner={{ Transparency: 0.5 }}
+			uiCornerDisabled={true}
+			uiStrokeOuterDisabled={true}
 		/>
 	);
 }
